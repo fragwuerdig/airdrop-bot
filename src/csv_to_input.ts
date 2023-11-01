99b07550dd7627eac0822e8980fa6a41129137c3
Till Ziegler <tz@schoeneweide.tk>
2023-11-01 08:52:22 +0100

    add: accept input and output as args

--- src/csv_to_input.ts
+++ src/csv_to_input.ts
@@ -1,17 +1,21 @@
 import * as fs from 'fs';
 import csvParser from 'csv-parser';
 
-const inputCSV = 'test-frag.csv';
-const outputJSON = 'output.json';
-
 const jsonArray = { list: [] };
 
 async function main() {
 
+    const inputCSV = process.argv[2]
+    const outputJSON = process.argv[3]
+
+    if ( !inputCSV || !outputJSON ) {
+        console.log("please provide input CSV file and output JSON file names")
+        return
+    }
+
     fs.createReadStream(inputCSV)
         .pipe(csvParser())
         .on('data', (row) => {
-            console.log(row)
             jsonArray.list.push({
                 wallet: row.wallet,
                 amount: parseInt(row.amount)
